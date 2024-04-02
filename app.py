d9db1ca28d32c78369faed38b4373f9ebe19395c
3sachi7 <157441212+3sachi7@users.noreply.github.com>
2024-04-02 11:45:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def video_reply():
     "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
     input={
         "prompt": q,
-        "num_frames": 20
+        "num_frames": 5
     },
 )
     time.sleep(60)
